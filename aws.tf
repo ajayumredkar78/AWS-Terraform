0a7806369418444f3f7a9c667f715d9da32d0d93
Ajay Umredkar <117751573+ajayumredkar78@users.noreply.github.com>
2022-11-08 21:19:34 +0530

    Add files via upload

---
+++ aws.tf
@@ -0,0 +1,5 @@
+provider "aws" {
+	access_key = "AKIAX6XMLZ7SHAWKNGEN"
+	secret_key = "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"
+	region ="us-east-1"
+}
---
+++ main.tf
@@ -0,0 +1,49 @@
+resource "aws_vpc" "main" {
+  cidr_block = "172.16.0.0/16"
+  instance_tenancy = "default"
+  tags = {
+    Name = "main"
+  }
+}
+
+#Create security group with firewall rules
+resource "aws_security_group" "jenkins-sg-2022" {
+  name        = var.security_group
+  description = "security group for jenkins"
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+ ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+ # outbound from Jenkins server
+  egress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags= {
+    Name = var.security_group
+  }
+}
+
+resource "aws_instance" "myFirstInstance" {
+  ami           = var.ami_id
+  key_name = var.key_name
+  instance_type = var.instance_type
+  vpc_security_group_ids = [aws_security_group.jenkins-sg-2022.id]
+  tags= {
+    Name = var.tag_name
+  }
+}
