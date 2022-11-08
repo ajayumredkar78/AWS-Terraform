61a026a5cf0c3f78ad163b6e092ebcc6d3feeb3b
Ajay Umredkar <117751573+ajayumredkar78@users.noreply.github.com>
2022-11-08 21:13:29 +0530

    Update and rename aws.tf to aws_setup.tf

--- aws.tf
+++
@@ -1,5 +0,0 @@
-provider "aws" {
-	access_key = "AKIAX6XMLZ7SHAWKNGEN"
-	secret_key = "WZOizXBtYDZrQmGZFh0jM3K5w58YxKOJUksPBDQU"
-	region ="us-east-1"
-}
---
+++ aws_setup.tf
@@ -0,0 +1,5 @@
+provider "aws" {
+	access_key = "Your Access Key Here"
+	secret_key = "Your Secret Key Here"
+	region ="Put your region ex.us-east-1"
+}
